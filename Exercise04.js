41ace60e9191916de09217854c36572394090549
rudy-rojas <rudy.rs1@gmail.com>
2023-01-07 02:28:32 -0400

    Exercise 04 Done

---
+++ Exercise04.js
@@ -0,0 +1,30 @@
+/*
+  4) Given an array of all your wishlist items, figure out 
+  how much it would cost to just buy everything at once.
+
+  function shoppingSpree(arr) {
+   // your code here    
+  }
+
+  var wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+  ];
+
+  console.log(shoppingSpree(wishlist)); // 227005
+*/
+
+const shoppingSpree = (arr) => arr.reduce((acc, el) => acc + el.price, 0);
+
+var wishlist = [
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
+];
+
+console.log(shoppingSpree(wishlist)); // 227005
